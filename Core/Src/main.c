782ecbe04a9802dcb9c1366a2f4676cc0826c73c
eden <edensheiko@gmail.com>
2023-03-27 13:13:58 +0300

    fixed code

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -197,13 +197,13 @@ static void MX_I2C1_Init(void)
   /* USER CODE END I2C1_Init 1 */
   hi2c1.Instance = I2C1;
   hi2c1.Init.Timing = 0x00808CD2;
-  hi2c1.Init.OwnAddress1 = 32;
+  hi2c1.Init.OwnAddress1 = 20;
   hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
   hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
   hi2c1.Init.OwnAddress2 = 0;
   hi2c1.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
-  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
-  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_ENABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_ENABLE;
   if (HAL_I2C_Init(&hi2c1) != HAL_OK)
   {
     Error_Handler();
--- RTG/Src/i2c.c
+++ RTG/Src/i2c.c
@@ -1,7 +1,7 @@
 #include "RTG.h"
 
 // Global flag to indicate successful I2C slave receive callback
-volatile uint8_t i2c2_slave_callback_success = FALSE;
+volatile uint8_t i2c_slave_callback_success = FALSE;
 
 /**
  * @brief Receive data over I2C and print the received buffer.
@@ -12,24 +12,15 @@ volatile uint8_t i2c2_slave_callback_success = FALSE;
 void receive_data_i2c() {
     uint8_t buffer_receive[BUFFER_SIZE];
 
-    // Receive data from I2C master asynchronously
-    HAL_StatusTypeDef status = HAL_I2C_Slave_Receive_IT(I2C_SLAVE, buffer_receive, BUFFER_SIZE);
+    // Receive data from I2C master synchronously
+    HAL_StatusTypeDef status = HAL_I2C_Slave_Receive(I2C_SLAVE, buffer_receive, BUFFER_SIZE, HAL_MAX_DELAY);
 
-    // Print received buffer
-    printf("%s\n\r", buffer_receive);
-}
-
-/**
- * @brief Callback function for I2C slave receive completion.
- *
- * This function is called when the I2C slave has finished receiving data.
- * It sets the i2c2_slave_callback_success flag to TRUE if the receive
- * operation was successful.
- *
- * @param hi2c I2C handle for the callback
- */
-void HAL_I2C_SlaveRxCpltCallback(I2C_HandleTypeDef *hi2c) {
-    if (hi2c == I2C_SLAVE) {
-        i2c2_slave_callback_success = TRUE;
+    // Check for successful reception
+    if (status == HAL_OK) {
+        // Print received buffer
+        printf("%s\n\r", buffer_receive);
+    } else {
+        // Print error message if data reception failed
+        printf("Error receiving data over I2C: %d\n\r", status);
     }
 }
